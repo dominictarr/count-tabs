38d00a1c47f075efb4feb462aba96951c88e7b86
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-14 14:05:03 -0500

    update example

--- example/client.js
+++ example/client.js
@@ -3,6 +3,7 @@
 var tabs = require('..')(function () {
   console.log(this.up)
   document.getElementById('tabs_counter').innerText = this.up
+  document.getElementById('tab_id').innerText = this.id
 })
 
 
